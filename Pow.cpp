36248670012cda8a8000e4e9b7e4fc260b2915d7
Nguyenduchien1206 <hien.nguyenduc2001@gmail.com>
2024-11-09 11:49:01 +0700

    Create Pow.cpp

---
+++ Pow.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    unordered_map<long long,double> memory;
+
+    double myPow(double x, long long n) {
+        if(n == 0){
+            return 1;
+        }
+
+        if(x==0){
+            return 0;
+        }
+
+        if(n<0){
+            x = 1/x;
+            n = n*-1;
+        }
+
+        if(memory.find(n) != memory.end()){
+            return memory[n];
+        }else{
+
+           
+            double result;
+            if(n%2==0){
+                 result = myPow(x,n/2) * myPow(x,n/2);
+                 memory[n] = result;
+                 return result;
+            }else{
+
+                result = x * myPow(x,n-1);
+                memory[n] = result;
+                return result;
+            }
+           
+            
+        }
+
+
+        
+    }
+};
